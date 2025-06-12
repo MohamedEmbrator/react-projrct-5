58b6736cb39c9f37908ae5ad69578a2c0bc58460
Mohamed Embrator <128060291+MohamedEmbrator@users.noreply.github.com>
2025-06-12 14:09:23 +0300

    Update productsApi.js

--- frontend/src/Redux/productsApi.js
+++ frontend/src/Redux/productsApi.js
@@ -3,20 +3,20 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Get All Data
 export const productsApi = createApi({
   reducerPath: "productsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://mohamedembrator.github.io/json-server/" }),
   endpoints: (builder) => ({
     getProductsByName: builder.query({
-      query: (name) => `products`
+      query: (name) => `index.json`
     })
   })
 });
 // Get one Product only
 export const oneProductApi = createApi({
   reducerPath: "oneProductApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://mohamedembrator.github.io/json-server/" }),
   endpoints: (builder) => ({
     getOneProduct: builder.query({
-      query: (id) => `products/${id}`
+      query: (id) => `/${id}.json`
     })
   })
 });
